b52e08add9d7b4cabe30b8d8238c7ae1d43ee3ab
Alix Shuk Han Chau <86642948+Alixchau@users.noreply.github.com>
2022-05-02 22:24:02 -0600

    added testimonial and footer section

---
+++ main.js
@@ -0,0 +1,11 @@
+//swiper js testmonial section
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+});
